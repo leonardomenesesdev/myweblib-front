69db0df34d32e2386b6e92c1d3b157e6c2e5e96b
leonardomenesesdev <leonardomenesesc@edu.unifor.br>
2025-11-13 13:25:57 -0300

    feat: signup component

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import SignUp from './components/SignUp'
+import Registration from './pages/Registration/Registration'
+
+function App() {
+
+  return (
+    <>
+    <Registration></Registration>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Registration/Registration.tsx
@@ -0,0 +1,7 @@
+import SignUp from "../../components/SignUp";
+
+export default function Registration(){
+    return(
+        <SignUp></SignUp>
+    )
+}
